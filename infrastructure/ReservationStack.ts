ad8e8bbf034c3e48c730b65c8fc038bc80d97dbe
cvial <cvial@dies.media>
2022-03-26 23:57:45 -0400

    Create a web API

--- infrastructure/ReservationStack.ts
+++ infrastructure/ReservationStack.ts
@@ -1,8 +1,27 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 export class ReservationStack extends Stack {
+  private api = new RestApi(this, "ReservationApi");
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
+
+    // Hello Api lambda integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
