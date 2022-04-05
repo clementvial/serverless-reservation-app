91ac8052d83847a47187ca973dc4160cefff2528
cvial <cvial@dies.media>
2022-04-04 20:38:39 -0400

    Refactor GenericTable class

--- infrastructure/ReservationStack.ts
+++ infrastructure/ReservationStack.ts
@@ -14,23 +14,24 @@ import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class ReservationStack extends Stack {
   private api = new RestApi(this, "ReservationApi");
-  private reservationTable = new GenericTable(
-    "ReservationTable",
-    "reservationId",
-    this
-  );
+
+  private reservationTable = new GenericTable(this, {
+    tableName: "ReservationTable",
+    primaryKey: "reservationId",
+    createLambdaPath: 'Create'
+  });
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
     const nodeLambda = new NodejsFunction(this, "helloLambdaNodeJs", {
-      entry:join(__dirname, "..", "services", "node-lambda", "hello.ts"),
+      entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     });
 
     const s3ListPolicy = new PolicyStatement();
-    s3ListPolicy.addActions('s3:ListAllMyBuckets');
-    s3ListPolicy.addResources('*');
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources("*");
 
     nodeLambda.addToRolePolicy(s3ListPolicy);
 
@@ -38,5 +39,9 @@ export class ReservationStack extends Stack {
     const helloLambdaIntegration = new LambdaIntegration(nodeLambda);
     const helloLambdaResource = this.api.root.addResource("hello");
     helloLambdaResource.addMethod("GET", helloLambdaIntegration);
+
+    // Reservation API integration
+    const reservationResources = this.api.root.addResource('reservations');
+    reservationResources.addMethod('POST', this.reservationTable.createLambdaIntegration);
   }
 }
